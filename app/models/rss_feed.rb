4812aac05c31476f4567dbd5fac323191347a5ed
Sushma Nivargi <sushmahpandey@gmail.com>
2016-03-02 09:56:27 +0530

    minor change for testing

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -8,15 +8,17 @@ class RssFeed < ActiveRecord::Base
 		def populate_from_xml(content)
 			xml_feeds = Nokogiri.XML(content)
 			xml_feeds.xpath("//item").each do |item|
-				RssFeed.create!(title: item.search('title').text, summary: item.search('description').text.gsub(/<[^>]*>/,''), published_at: item.search('pubDate').text.to_datetime, url: item.search('link').text)
+				RssFeed.create(title: item.search('title').text, summary: item.search('description').text.gsub(/<[^>]*>/,''), published_at: item.search('pubDate').text.to_datetime, url: item.search('link').text)
 			end
 		end
 	
 		def notify_rss_feed_creation
-			RssFeed.connection.execute "LISTEN rss_feeds_channel"
+			#RssFeed.connection.execute "LISTEN rss_feeds_channel"
 			#Wait for notification or timeout and return nil
-			RssFeed.connection.raw_connection.wait_for_notify(NOTIFY_TIMEOUT) do |event, pid, rss_feed_id|
-				yield rss_feed_id
+			loop do
+				RssFeed.connection.raw_connection.wait_for_notify(NOTIFY_TIMEOUT) do |event, pid, rss_feed_id|
+					yield rss_feed_id
+				end
 			end
 		ensure
 			RssFeed.connection.execute "UNLISTEN rss_feeds_channel"
@@ -28,6 +30,7 @@ class RssFeed < ActiveRecord::Base
 	########
 	
 	def notify_rss_feed_created
+		RssFeed.connection.execute "LISTEN rss_feeds_channel"
 		RssFeed.connection.execute "NOTIFY rss_feeds_channel, '#{self.id}'"
 	end
 	
